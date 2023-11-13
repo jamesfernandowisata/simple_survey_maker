50dee77bc469909d92e216c0afe878d0309f1bdd
James Fernando <jamesfernando1311@gmail.com>
2023-11-13 20:39:57 +0700

    final

--- src/pages/SurveyComponent.jsx
+++ src/pages/SurveyComponent.jsx
@@ -5,11 +5,27 @@ import "survey-core/defaultV2.min.css";
 import * as SurveyTheme from "survey-core/themes";
 import "../index.css";
 import { json } from "./json";
+import { themeJson } from "./theme";
+
+
 
 function SurveyComponent() {
+   
     const survey = new Model(json);
+    survey.applyTheme(themeJson);
     survey.onComplete.add((sender, options) => {
-        console.log(JSON.stringify(sender.data, null, 3)); //notes what to do with the data
+        var jsonfile = JSON.stringify(sender.data, null, 3);
+        const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+            jsonfile
+          )}`;
+          const link = document.createElement("a");
+          link.href = jsonString;
+          link.download = "data.json";
+          link.click();
+        
+          setTimeout(function(){
+            window.location.reload();
+         }, 5000);
     });
     return (<Survey model={survey} />);
 }
