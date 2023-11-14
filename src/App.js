415ad7cfdb964ca984b101b0e17e3feb683d6c4a
jamesfernandowisata <jamesfernando1311@gmail.com>
2023-11-14 21:06:38 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from '../src/pages/Home';
+import SurveyComponent from '../src/pages/SurveyComponent';
+import result from '../src/pages/result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/" exact component={Home} />
+      <Route path="/survey" component={SurveyComponent} />
+      <Route path="/result" component={ContactPage} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import SurveyComponent from '../src/pages/SurveyComponent';
+import App from '../App.js';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <SurveyComponent />
-  </React.StrictMode>
+  <Router>
+      <App />
+  </Router>,
+
 );
 
 // If you want to start measuring performance in your app, pass a function
